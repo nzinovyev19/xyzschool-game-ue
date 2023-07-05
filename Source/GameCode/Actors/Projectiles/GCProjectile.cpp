546ac336bbe947f7f36baa3e38b316abecc35bf2
Nikita Zinovyev <nrz2000@mail.ru>
2023-07-05 22:50:13 +0300

    feat: create GCProjectile class and grenade blueprint based on him

---
+++ Source/GameCode/Actors/Projectiles/GCProjectile.cpp
@@ -0,0 +1,19 @@
+#include "GCProjectile.h"
+
+AGCProjectile::AGCProjectile()
+{
+	CollisionComponent = CreateDefaultSubobject<USphereComponent>(TEXT("CollisionComponent"));
+	CollisionComponent->InitSphereRadius(5.0f);
+	CollisionComponent->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
+	CollisionComponent->SetCollisionResponseToAllChannels(ECR_Block);
+	SetRootComponent(CollisionComponent);
+
+	ProjectileMovementComponent = CreateDefaultSubobject<UProjectileMovementComponent>(TEXT("MovementComponent"));
+	ProjectileMovementComponent->InitialSpeed = 2000.0f;
+}
+
+void AGCProjectile::LaunchProjectile(FVector Direction)
+{
+	ProjectileMovementComponent->Velocity = Direction * ProjectileMovementComponent->InitialSpeed;
+	CollisionComponent->IgnoreActorWhenMoving(GetOwner(), true);
+}
---
+++ Source/GameCode/Actors/Projectiles/GCProjectile.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/SphereComponent.h"
+#include "GameFramework/Actor.h"
+#include "GameFramework/ProjectileMovementComponent.h"
+#include "GCProjectile.generated.h"
+
+UCLASS()
+class GAMECODE_API AGCProjectile : public AActor
+{
+	GENERATED_BODY()
+	
+public:
+	AGCProjectile();
+
+	UFUNCTION(BlueprintCallable)
+	void LaunchProjectile(FVector Direction);
+
+protected:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
+	USphereComponent* CollisionComponent;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
+	UProjectileMovementComponent* ProjectileMovementComponent;
+
+};
