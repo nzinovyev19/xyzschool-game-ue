3eb9a3864b6ef4faabba7aabdfcf17f850f9fb1a
Nikita Zinovyev <nrz2000@mail.ru>
2023-08-20 14:15:50 +0300

    feat: implement base logic of inventory-slot widget (handle mouse actions for interacting)

--- Source/GameCode/UI/Widgets/Inventory/InventorySlotWidget.cpp
+++ Source/GameCode/UI/Widgets/Inventory/InventorySlotWidget.cpp
@@ -1,5 +1,118 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
 #include "InventorySlotWidget.h"
 
+#include "Blueprint/WidgetBlueprintLibrary.h"
+#include "Components/Image.h"
+#include "GameCode/Characters/GCBaseCharacter.h"
+#include "GameCode/Components/CharacterComponents/CharacterInventoryComponent.h"
+#include "GameCode/Inventory/Items/InventoryItem.h"
+
+void UInventorySlotWidget::InitializeItemSlot(FInventorySlot& InventorySlot)
+{
+	LinkedSlot = &InventorySlot;
+
+	FInventorySlot::FInventorySlotUpdate OnInventorySlotUpdate;
+	OnInventorySlotUpdate.BindUObject(this, &UInventorySlotWidget::UpdateView);
+	LinkedSlot->BindOnInventorySlotUpdate(OnInventorySlotUpdate);
+}
+
+void UInventorySlotWidget::UpdateView()
+{
+	if (LinkedSlot == nullptr)
+	{
+		ImageItemIcon->SetBrushFromTexture(nullptr);
+		// Count = 0;
+		return;
+	}
+
+	if (LinkedSlot->Item.IsValid())
+	{
+		const FInventoryItemDescription& Description = LinkedSlot->Item->GetDescription();
+		ImageItemIcon->SetBrushFromTexture(Description.Icon);
+		// Count = LinkedSlot->Item->Amount;
+	}
+	else
+	{
+		ImageItemIcon->SetBrushFromTexture(nullptr);
+		// Count = 0;
+	}
+}
+
+FReply UInventorySlotWidget::NativeOnMouseButtonDown(const FGeometry& InGeometry, const FPointerEvent& InMouseEvent)
+{
+	if (LinkedSlot == nullptr)
+	{
+		return FReply::Handled();
+	}
+
+	if (!LinkedSlot->Item.IsValid())
+	{
+		return FReply::Handled();
+	}
+
+	FKey MouseBtn = InMouseEvent.GetEffectingButton();
+	if (MouseBtn == EKeys::RightMouseButton)
+	{
+		/* Some simplification, so as not to complicate the architecture
+		 * - on instancing item, we use the current pawn as an outer one.
+		 * In real practice we need use callback for inform item holder what action was do in UI */
+
+		TWeakObjectPtr<UInventoryItem> LinkedSlotItem = LinkedSlot->Item;
+		AGCBaseCharacter* ItemOwner = Cast<AGCBaseCharacter>(LinkedSlotItem->GetOuter());
+
+		if (LinkedSlotItem->IsConsumable() && LinkedSlotItem->Consume(ItemOwner))
+		{
+			LinkedSlot->ClearSlot();
+		}
+		return FReply::Handled();
+	}
+
+	FEventReply Reply = UWidgetBlueprintLibrary::DetectDragIfPressed(InMouseEvent, this, EKeys::LeftMouseButton);
+	return Reply.NativeReply;
+}
+
+void UInventorySlotWidget::NativeOnDragDetected(const FGeometry& InGeometry, const FPointerEvent& InMouseEvent, UDragDropOperation*& OutOperation)
+{
+	UDragDropOperation* DragOperation = Cast<UDragDropOperation>(UWidgetBlueprintLibrary::CreateDragDropOperation(UDragDropOperation::StaticClass()));
+
+	/* Some simplification for not define new widget for drag and drop operation  */
+	UInventorySlotWidget* DragWidget = CreateWidget<UInventorySlotWidget>(GetOwningPlayer(), GetClass());
+	DragWidget->ImageItemIcon->SetBrushFromTexture(LinkedSlot->Item->GetDescription().Icon);
+	// DragWidget->Count = LinkedSlot->Item->Amount;
+
+	DragOperation->DefaultDragVisual = DragWidget;
+	DragOperation->Pivot = EDragPivot::MouseDown;
+	DragOperation->Payload = LinkedSlot->Item.Get();
+	OutOperation = DragOperation;
+
+	LinkedSlot->ClearSlot();
+}
+
+bool UInventorySlotWidget::NativeOnDrop(const FGeometry& InGeometry, const FDragDropEvent& InDragDropEvent, UDragDropOperation* InOperation)
+{
+	if (!LinkedSlot->Item.IsValid())
+	{
+		LinkedSlot->Item = TWeakObjectPtr<UInventoryItem>(Cast<UInventoryItem>(InOperation->Payload));
+		LinkedSlot->UpdateSlotState();
+		return true;
+	}
+
+	return false;
+}
+
+void UInventorySlotWidget::NativeOnDragCancelled(const FDragDropEvent& InDragDropEvent, UDragDropOperation* InOperation)
+{
+	// UInventoryItem* Item = Cast<UInventoryItem>(InOperation->Payload);
+	// //if (IsValid(Item) && Item->Spawn(Cast<AActor>(Item->GetOuter())->GetTransform() + FTransform(FVector(100,0,0))))
+	// AActor* Owner = Cast<AActor>(Item->GetOuter());
+	// if (IsValid(Item) && Item->Spawn(FTransform(Owner->GetActorLocation() + Owner->GetActorForwardVector() * 100)))
+	// {
+	// 	return;
+	// }
+	LinkedSlot->Item = TWeakObjectPtr<UInventoryItem>(Cast<UInventoryItem>(InOperation->Payload));
+	LinkedSlot->UpdateSlotState();
+}
+
+// void UInventorySlotWidget::SetItemIcon(UTexture2D* Icon)
+// {
+// 	ImageItemIcon->SetBrushFromTexture(Icon);
+// }
--- Source/GameCode/UI/Widgets/Inventory/InventorySlotWidget.h
+++ Source/GameCode/UI/Widgets/Inventory/InventorySlotWidget.h
@@ -6,12 +6,32 @@
 #include "Blueprint/UserWidget.h"
 #include "InventorySlotWidget.generated.h"
 
-/**
- * 
- */
+struct FInventorySlot;
+class UImage;
+
 UCLASS()
 class GAMECODE_API UInventorySlotWidget : public UUserWidget
 {
 	GENERATED_BODY()
+
+public:
+	void InitializeItemSlot(FInventorySlot& InventorySlot);
+	void UpdateView();
+	// void SetItemIcon(UTexture2D* Icon);
+
+protected:
+	UPROPERTY(meta = (BindWidget))
+	UImage* ImageItemIcon;
+
+	// UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	// int32 Count;
+
+	virtual FReply NativeOnMouseButtonDown(const FGeometry& InGeometry, const FPointerEvent& InMouseEvent) override;
+	virtual void NativeOnDragDetected(const FGeometry& InGeometry, const FPointerEvent& InMouseEvent, UDragDropOperation*& OutOperation) override;
+	virtual bool NativeOnDrop(const FGeometry& InGeometry, const FDragDropEvent& InDragDropEvent, UDragDropOperation* InOperation) override;
+	virtual void NativeOnDragCancelled(const FDragDropEvent& InDragDropEvent, UDragDropOperation* InOperation) override;
+
+private:
+	FInventorySlot* LinkedSlot;
 	
 };
