7a25afd32c573fd0ce9b8997f976fd1ff44a0a68
Nikita Zinovyev <nrz2000@mail.ru>
2023-09-04 10:42:52 +0300

    feat: create all required classes of streaming-subsystem

---
+++ Source/GameCode/Subsystems/StreamingSubsystem/StreamingSubsystemUtils.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "StreamingSubsystemUtils.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GAMECODE_API UStreamingSubsystemUtils : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+	
+};
